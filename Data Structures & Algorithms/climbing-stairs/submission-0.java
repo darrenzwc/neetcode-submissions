ec1c0e9fd1aceca39d345a27a7954c4bf49f8aa8
darrenzwc <136639184+darrenzwc@users.noreply.github.com>
2026-09-24 16:15:44 -0500

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+
+        // since if we go from top down and choose to subtract 1 or 2 
+        return step(n, dp);
+    }
+
+    private static int step(int n, int[] dp) {
+        // see if already stored
+        if(n < 0) {
+            return 0;
+        }
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+        if(dp[n] != 0) {
+            return dp[n];
+        }
+        dp[n] = step(n - 1, dp) + step(n - 2, dp);
+        return dp[n];
+        
+    }
+}
